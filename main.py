3b141128b8532a4bc2ab3a013b3979a1fd35afc9
Manh Dung DO <mddo@babel.cluster.lan>
2020-01-16 18:58:54 +0100

    commit from ssh vs

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from help_function import generate_file
 
 
 def main():
-    #insertion_pos_file = "PourMD/data/CLQCA_20-184-all-rel_readPerPos.txt"
-    #gff_file = "PourMD/Ref/all_subtracts_10kbNI_genes.bed"
-    #save_file = "hits_count_per_10kbNI_genes_CLQCA20184.out"
-    #generate_file(insertion_pos_file, gff_file, save_file)
-    print("check pull")
-
-    print("dung qsdsqdsdqdq vscode")   
+    insertion_pos_file = "PourMD/data/CLQCA_20-184-all-rel_readPerPos.txt"
+    gff_file = "PourMD/Ref/all_subtracts_10kbNI_genes.bed"
+    save_file = "test.out"
+    generate_file(insertion_pos_file, gff_file, save_file)
 if __name__ == "__main__":
     main()
