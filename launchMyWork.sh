f51068396ee578f6a0010802ba50963d147c6369
Manh Dung DO <mddo@babel.cluster.lan>
2020-01-24 16:35:17 +0100

    create FY data

--- launchMyWork.sh
+++ launchMyWork.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -p fast ###la partition sur laquelle on soumet le job
-#SBATCH -n 1 ##le nombre de coeurs alloués
+#SBATCH -n 2 ##le nombre de coeurs alloués
 #SBATCH --mem-per-cpu=2000 ###la mémoire requise par CPU (pas sûr que ce paramètre soit réllement utile
 #SBATCH -o /home/mddo/stage/M2S4/logs/slurmPools.%N.%j.out   ###position du log d'output
 #SBATCH -e /home/mddo/stage/M2S4/logs/slurmPools.%N.%j.err   ###position du log d'erreur
